2d5ea6c73403b589b7e177de14c53fe51c466c17
Pablo Marti <startpablo@gmail.com>
2015-05-21 10:11:23 -0600

    New config for pdf

---
+++ config/initializers/pdfkit.rb
@@ -0,0 +1,4 @@
+PDFKit.configure do |config|
+  config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+  # config.root_url = "http://staging.myapp.com" # Use only if your external hostname is unavailable on the server.
+end
